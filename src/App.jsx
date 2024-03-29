afc4d331f25bf9a263bf08ed845b4e58212d8890
dtschleckser <dtschleckser@gmail.com>
2024-03-28 21:01:45 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h2>
+      Sparse autoencoders and mechanistic interpretability
+      </h2>
+
+    </div>
+  )
+}
+
+export default App
