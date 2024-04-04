1539ac1395d170ee22bf12470533f9334d3122dd
dtschleckser <dtschleckser@gmail.com>
2024-04-04 19:24:50 -0400

    Separate github io build process

--- src/pages/BlogRoot.jsx
+++ src/pages/BlogRoot.jsx
@@ -1,6 +1,8 @@
 import moeHeaderArt from '../assets/blog-headers/moe-gradient.png'
 import BlogListItem from '../components/BlogListItem/BlogListItem'
 
+import { FaLinkedin, FaTwitter } from "react-icons/fa6";
+
 function BlogRoot() {
     return <div>
         <h2>Posts</h2>
@@ -14,6 +16,14 @@ function BlogRoot() {
                 />
             </li>
         </ul>
+        <div style={{position: 'fixed', left: '5px', bottom: '5px'}}>
+            <a href="https://www.linkedin.com/in/daniel-schleckser/" style={{marginRight: '5px'}}>
+                <FaLinkedin size={25}/>
+            </a>
+            <a href="https://www.twitter.com/dtschleckser">
+                <FaTwitter size={25}/>
+            </a>
+        </div>
     </div>
 }
 
